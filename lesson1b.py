afe157a2bece188989f92d835b1769914959c28b
wangui7_jpegecho <marthawaruix7@gmail.com>
2026-02-08 23:17:25 +0300

    added lesson 1b

--- lesson1b.py
+++ lesson1b.py
@@ -1,2 +1,18 @@
 #Python Data Types
-#String-cd 
+#String-it is a series of characters that are inside of qoutes
+name="Joseph McMuga"
+print(type(name))
+
+#Numeric
+#Int-this indicates whole numbers in python.For an integer ,we dont use quote
+
+age="39"
+print("the age of  person is: ",age)
+print(type(age))
+
+#float-this is a number with a decimal
+coordinates = 15.4524
+print("The coordinates are: ", coordinates)
+print(type(coordinates))
+
+#Research an come up with an example of a complex data type
